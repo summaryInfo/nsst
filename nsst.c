04c3709320b4e8d458b20df16d1223d5d2868132
Baskov Evgeniy <j-basevgser@yandex.ru>
2020-08-16 05:02:12 +0300

    v1.5.2

--- nsst.c
+++ nsst.c
@@ -50,7 +50,7 @@ static _Noreturn void usage(char *argv0, int code) {
 }
 
 static _Noreturn void version(void) {
-    printf("Not So Simple Terminal v1.5.1\n"
+    printf("Not So Simple Terminal v1.5.2\n"
             "Features: nsst"
 #if USE_PPOLL
             "+ppoll"
