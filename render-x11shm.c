988a9754174eb2d9f258cbb51c0196cfce5a69c8
Baskov Evgeniy <j-basevgser@yandex.ru>
2020-09-02 21:07:37 +0300

    Unbreak MIT-SHM renderer

--- render-x11shm.c
+++ render-x11shm.c
@@ -151,11 +151,11 @@ bool renderer_reload_font(struct window *win, bool need_free) {
     find_shared_font(win, need_free);
 
     if (need_free) {
-        handle_resize(win, win->width, win->height);
+        handle_resize(win, win->cfg.width, win->cfg.height);
         window_set_default_props(win);
     } else {
-        win->cw = MAX(1, (win->width - 2*win->left_border) / win->char_width);
-        win->ch = MAX(1, (win->height - 2*win->top_border) / (win->char_height + win->char_depth));
+        win->cw = MAX(1, (win->cfg.width - 2*win->cfg.left_border) / win->char_width);
+        win->ch = MAX(1, (win->cfg.height - 2*win->cfg.top_border) / (win->char_height + win->char_depth));
 
         resize_bounds(win, 1);
 
@@ -235,7 +235,7 @@ static void optimize_bounds(struct rect *bounds, size_t *boundc, bool fine_grain
 bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, ssize_t cur_y, bool cursor, bool marg) {
 
     bool scrolled = win->ren.boundc;
-    bool cond_cblink = !win->blink_commited && (win->cursor_type & 1) && term_is_cursor_enabled(win->term);
+    bool cond_cblink = !win->blink_commited && (win->cfg.cursor_shape & 1) && term_is_cursor_enabled(win->term);
 
     if (cond_cblink) cursor |= win->blink_state;
 
@@ -257,7 +257,7 @@ bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, s
             if (dirty || next_dirty) {
 
                 if (k == cur_y && i == cur_x && cursor &&
-                        win->focused && ((win->cursor_type + 1) & ~1) == cusor_type_block)
+                        win->focused && ((win->cfg.cursor_shape + 1) & ~1) == cusor_type_block)
                     attr.reverse ^= 1;
 
                 spec = describe_cell(cel, attr, palette, &win->cfg, win->blink_state, mouse_is_selected_in_view(win->term, i, k));
@@ -268,7 +268,7 @@ bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, s
 
             if (dirty || (g_wide && next_dirty)) {
                 int16_t cw = win->char_width, ch = win->char_height;
-                int16_t cd = win->char_depth, ul = win->underline_width;
+                int16_t cd = win->char_depth, ul = win->cfg.underline_width;
                 int16_t x = i * cw, y = k * (ch + cd);
                 int16_t ls = win->cfg.line_spacing/2;
                 int16_t fs = win->cfg.font_spacing/2;
@@ -306,7 +306,7 @@ bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, s
                 if (mouse_is_selected_in_view(win->term, win->cw - 1, k)) {
                     c = palette[SPECIAL_SELECTED_BG];
                     if (!c) c = palette[SPECIAL_FG];
-                    c = color_apply_a(c, win->alpha);
+                    c = color_apply_a(c, win->cfg.alpha*255);
                 }
                 image_draw_rect(win->ren.im, (struct rect){
                     .x = line.width * win->char_width,
@@ -336,19 +336,19 @@ bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, s
         };
         size_t off = 0, count = 4;
         if (win->focused) {
-            if (((win->cursor_type + 1) & ~1) == cusor_type_bar) {
+            if (((win->cfg.cursor_shape + 1) & ~1) == cusor_type_bar) {
                 if (marg) {
                     off = 2;
-                    rects[2].width = win->cursor_width;
-                    rects[2].x -= win->cursor_width - 1;
+                    rects[2].width = win->cfg.cursor_width;
+                    rects[2].x -= win->cfg.cursor_width - 1;
                 } else
-                    rects[0].width = win->cursor_width;
+                    rects[0].width = win->cfg.cursor_width;
                 count = 1;
-            } else if (((win->cursor_type + 1) & ~1) == cusor_type_underline) {
+            } else if (((win->cfg.cursor_shape + 1) & ~1) == cusor_type_underline) {
                 count = 1;
                 off = 3;
-                rects[3].height = win->cursor_width;
-                rects[3].y -= win->cursor_width - 1;
+                rects[3].height = win->cfg.cursor_width;
+                rects[3].y -= win->cfg.cursor_width - 1;
             } else {
                 count = 0;
             }
@@ -373,14 +373,14 @@ bool window_submit_screen(struct window *win, color_t *palette, int16_t cur_x, s
 void renderer_update(struct window *win, struct rect rect) {
     if (rctx.has_shm_pixmaps) {
         xcb_copy_area(con, win->ren.shm_pixmap, win->wid, win->gc, rect.x, rect.y,
-                rect.x + win->left_border, rect.y + win->top_border, rect.width, rect.height);
+                rect.x + win->cfg.left_border, rect.y + win->cfg.top_border, rect.width, rect.height);
     } else if (rctx.has_shm) {
         xcb_shm_put_image(con, win->wid, win->gc, win->ren.im.width, win->ren.im.height, rect.x, rect.y, rect.width, rect.height,
-                rect.x + win->left_border, rect.y + win->top_border, 32, XCB_IMAGE_FORMAT_Z_PIXMAP, 0, win->ren.shm_seg, 0);
+                rect.x + win->cfg.left_border, rect.y + win->cfg.top_border, 32, XCB_IMAGE_FORMAT_Z_PIXMAP, 0, win->ren.shm_seg, 0);
     } else {
         xcb_put_image(con, XCB_IMAGE_FORMAT_Z_PIXMAP, win->wid, win->gc,
-                win->ren.im.width, rect.height, win->left_border,
-                win->top_border + rect.y, 0, 32, rect.height * win->ren.im.width * sizeof(color_t),
+                win->ren.im.width, rect.height, win->cfg.left_border,
+                win->cfg.top_border + rect.y, 0, 32, rect.height * win->ren.im.width * sizeof(color_t),
                 (const uint8_t *)(win->ren.im.data+rect.y*win->ren.im.width));
     }
 }
