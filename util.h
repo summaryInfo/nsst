bc40b6d8a4ed0a53e5f5eeb0abff512b78be6768
Baskov Evgeniy <j-basevgser@yandex.ru>
2022-04-10 16:47:53 +0300

    Add rounding helpers

--- util.h
+++ util.h
@@ -12,9 +12,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define ROUNDUP(x, align) (((x) + (align) - 1) & ~((align) - 1))
+#define ROUNDDOWN(x, align) ((x) & ~(align))
+
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
+
 #define SWAP(a, b) do{__typeof__(a) t__ = (a); (a) = (b); (b) = t__;}while(0)
+
 #define SEC 1000000000LL
 #define CACHE_LINE 64
 #define TIMEDIFF(t, d)  ((((d).tv_sec - (t).tv_sec) * SEC + ((d).tv_nsec - (t).tv_nsec)))
