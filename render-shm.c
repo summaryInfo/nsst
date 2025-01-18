summaryInfo/nsst

commit 877ef1c7f7fab0dff0f68bb1477207c8e7dc1fab

Fix redrawing of empty wrapped lines

render-shm.c
@@ -121,6 +121,7 @@ bool shm_submit_screen(struct window *win, int16_t cur_x, ssize_t cur_y, bool cu
 
     struct screen *scr = term_screen(win->term);
     struct line_span span = screen_view(scr);
+    struct line *prev_line = NULL;
     for (ssize_t k = 0; k < win->c.height; k++, screen_span_shift(scr, &span)) {
         screen_span_width(scr, &span);
         bool next_dirty = false;
@@ -229,9 +230,13 @@ bool shm_submit_screen(struct window *win, int16_t cur_x, ssize_t cur_y, bool cu
         }
 
         /* Only reset force flag for last part of the line */
-        if (!view_wrapped(&span))
-            span.line->force_damage = false;
+        if (prev_line != span.line && prev_line) {
+            prev_line->force_damage = false;
+            prev_line = span.line;
+        }
     }
+    if (prev_line)
+        prev_line->force_damage = false;
 
     if (cursor_visible) {
         struct cursor_rects cr = describe_cursor(win, cur_x, cur_y, on_margin, beyond_eol);

render-xrender-x11.c
@@ -568,6 +568,7 @@ static bool prepare_multidraw(struct window *win, int16_t cur_x, ssize_t cur_y, 
 
     struct screen *scr = term_screen(win->term);
     struct line_span span = screen_view(scr);
+    struct line *prev_line = NULL;
     for (ssize_t k = 0; k < win->c.height; k++, screen_span_shift(scr, &span)) {
         screen_span_width(scr, &span);
         bool next_dirty = false, first_in_line = true;
@@ -767,9 +768,13 @@ static bool prepare_multidraw(struct window *win, int16_t cur_x, ssize_t cur_y, 
             next_dirty = dirty;
         }
         /* Only reset force flag for last part of the line */
-        if (!view_wrapped(&span))
-            span.line->force_damage = false;
+        if (prev_line != span.line && prev_line) {
+            prev_line->force_damage = false;
+            prev_line = span.line;
+        }
     }
+    if (prev_line)
+        prev_line->force_damage = false;
     return has_blinking;
 }
 

term.h
@@ -22,10 +22,6 @@ typedef int32_t iparam_t;
 
 #define SCNparam SCNu32
 
-static inline bool view_wrapped(struct line_span *view) {
-    return view->line->size > view->offset + view->width;
-}
-
 static inline struct cell *view_cell(struct line_span *view, ssize_t x) {
     return view->line->cell + view->offset + x;
 }

commit 716b4ade9ce1d903e0d7c0ad442666aeb1b83b55

Fix performance regression after 877ef1c
force_redraw flag were never reset.

render-shm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019-2022, Evgeniy Baskov. All rights reserved */
+/* Copyright (c) 2019-2022,2025, Evgeniy Baskov. All rights reserved */
 
 
 #include "feature.h"
@@ -230,10 +230,9 @@ bool shm_submit_screen(struct window *win, int16_t cur_x, ssize_t cur_y, bool cu
         }
 
         /* Only reset force flag for last part of the line */
-        if (prev_line != span.line && prev_line) {
+        if (prev_line != span.line && prev_line)
             prev_line->force_damage = false;
-            prev_line = span.line;
-        }
+        prev_line = span.line;
     }
     if (prev_line)
         prev_line->force_damage = false;

render-xrender-x11.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019-2022, Evgeniy Baskov. All rights reserved */
+/* Copyright (c) 2019-2022,2025, Evgeniy Baskov. All rights reserved */
 
 #include "feature.h"
 
@@ -768,10 +768,9 @@ static bool prepare_multidraw(struct window *win, int16_t cur_x, ssize_t cur_y, 
             next_dirty = dirty;
         }
         /* Only reset force flag for last part of the line */
-        if (prev_line != span.line && prev_line) {
+        if (prev_line != span.line && prev_line)
             prev_line->force_damage = false;
-            prev_line = span.line;
-        }
+        prev_line = span.line;
     }
     if (prev_line)
         prev_line->force_damage = false;