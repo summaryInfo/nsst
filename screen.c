summaryInfo/nsst

commit 4cd958516adc48cfb314bc9dd69eb984b2ac218c

Fix regression after c7f26a1
Reproducer:

printf '\033[8;29;81t'
sleep .1
printf '\033[999B\033[1mX\033[J'
printf '\033[8;29;80t'
sleep .1
printf '\nThis should be visible'
sleep 1000

screen.c
@@ -153,7 +153,6 @@ FORCEINLINE
 static inline void screen_split_line_after_ex(struct screen *scr, struct screen_storage *screen, ssize_t y) {
     struct line_span *src = screen->begin + y;
     ssize_t offset = src->offset + src->width;
-    if (LIKELY(src + 1 >= screen->end || src[1].line != src->line)) return;
 
     assert(offset <= src->line->size);
 
@@ -174,7 +173,10 @@ static inline void screen_split_line_before(struct screen *scr, ssize_t y) {
 
 FORCEINLINE
 static inline void screen_split_line_after(struct screen *scr, ssize_t y) {
-    screen_split_line_after_ex(scr, get_current_screen(scr), y);
+    struct screen_storage *screen = get_current_screen(scr);
+    struct line_span *src = screen->begin + y;
+    if (LIKELY(src + 1 >= screen->end || src[1].line != src->line)) return;
+    screen_split_line_after_ex(scr, screen, y);
 }
 
 static inline struct line *screen_realloc_line(struct screen *scr, struct line *line, ssize_t width, struct screen_storage *screen) {

commit 58e869d814f94f581532262710b5a24dbc267aad

Complete fixing regression together with 4cd9585

screen.c
@@ -771,7 +771,9 @@ enum stick_view resize_main_screen(struct screen *scr, ssize_t width, ssize_t he
 
         /* Truncate lines that are below the last line of the screen */
         if (y >= height) {
-            screen_split_line_after_ex(scr, &scr->main_screen, height -  1);
+            struct line_span *src = &scr->main_screen.begin[height - 1];
+            if (src->line->size != src->offset + src->width)
+                screen_split_line_after_ex(scr, &scr->main_screen, height -  1);
             free_line_list_until(scr, &scr->main_screen, screen[height - 1].line->next, NULL);
             y = height;
         }